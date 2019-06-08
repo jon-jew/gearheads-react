1749535fd5ce15600d7d21f11e94ea855cbad080
fireteam99 <raymancheungsy@gmail.com>
2019-06-07 21:03:50 -0400

    created config folder and added a private key to connect to firebase

--- gearheads-react-api/server.js
+++ gearheads-react-api/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+
 const admin = require('firebase-admin');
+const serviceAccount = require("../config/serviceAccountKey.json");
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -9,6 +12,11 @@ app.use(bodyParser.json({ extended: true}));
 app.use(cors());
 
 // connect to firebase
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://gearheads-1529515255083.firebaseio.com"
+});
+
 
 app.get('/', (req, res) => {
     res.send('test');
